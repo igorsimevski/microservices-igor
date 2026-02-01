dfc8819d33ae48c78489ea6fc1b3c6767f2791a6
igorsimevski <igor.simevski@vasscompany.com>
2026-02-01 17:59:14 +0100

    Fixing update loan

--- loans/src/main/java/com/igor/loans/dto/LoansDto.java
+++ loans/src/main/java/com/igor/loans/dto/LoansDto.java
@@ -1,10 +1,12 @@
 package com.igor.loans.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class LoansDto {
   private Long loanId;
   private String mobileNumber;
